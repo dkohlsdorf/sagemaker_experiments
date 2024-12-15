032307bc5470e58fde0862959b98a81a15fc3db4
dkohlsdorf <dkohlsdorf@gmail.com>
2024-12-15 21:43:05 +0000

    preprocessing job fixes

--- pipelines/demand_prediction_ar_sagemaker/preprocessing_job.py
+++ pipelines/demand_prediction_ar_sagemaker/preprocessing_job.py
@@ -13,35 +13,43 @@ def schedule():
 
     processing_instance_type = "ml.m5.xlarge"
     processing_instance_count = 1
+    bucket = sess.default_bucket()
+    print(f" >>>>> {bucket} {role}")
+
+    input_uri=f's3://{bucket}/historic.py'
+    print(f" >>>>> {input_uri}")
 
     processor = SKLearnProcessor(
         framework_version="0.20.0",
         role=role,
         instance_type=processing_instance_type,
         instance_count=processing_instance_count,
-        max_runtime_in_seconds=7200,
     )
     processor.run(
         code="preprocessing.py",
         inputs=[
             ProcessingInput(
-                source='s3://dkohlsdorf-experiments/historic.csv',
+                source=input_uri,
                 destination="/opt/ml/processing/input/data/",
             )
         ],
         outputs=[
             ProcessingOutput(
-                output_name="train_data", source="/opt/ml/processing/train"
+                output_name="train_data", source="/opt/ml/processing/output"
             ),
         ],
         arguments=[
             "--input",
             "/opt/ml/processing/input/data/historical.csv",
-            "--output"
+            "--output",
             "/opt/ml/processing/output/x_notebook.csv"
-        ],
+        ]
     )
     preprocessing_job_description = sklearn_processor.jobs[-1].describe()
+    output_config = preprocessing_job_description["ProcessingOutputConfig"]
+    for output in output_config["Outputs"]:
+        if output["OutputName"] == "train_data":
+            preprocessed_training_data = output["S3Output"]["S3Uri"]
 
 
 if __name__== '__main__':
